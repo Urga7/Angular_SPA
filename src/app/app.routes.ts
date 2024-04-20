4108f28a53f1beedab9ac684dd1a393d4ddfd372
Urga7 <urban.gajsek@gmail.com>
2024-04-20 19:23:42 +0200

    Added initial components and implemented routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { SettingsComponent } from './settings/settings.component';
+import { UsersComponent } from './users/users.component';
+import { AbsencesComponent } from './absences/absences.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'settings', title: 'Settings', component: SettingsComponent },
+  { path: 'users', title: 'Users', component: UsersComponent },
+  { path: 'absences', title: 'Absences', component: AbsencesComponent },
+  { path: '', redirectTo: '/settings', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
